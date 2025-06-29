26519f9c81d076202857979cbfe4e74c924446a2
Gaston Pappaterra <gpappaterra@hotmail.com>
2025-06-29 00:52:36 -0300

    Instalo Joi y creo el validationSchema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [ EnvConfiguration ]
+      load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema
     }),
-
 
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,5 +3,5 @@ export const EnvConfiguration = () => ({
   enviroment: process.env.NODE_ENV || 'dev',
   mongoDB: process.env.MONGODB,
   port: process.env.PORT || 3002,
-  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+  defaultLimit: Number( process.env.DEFAULT_LIMIT ) || 7,
 });
